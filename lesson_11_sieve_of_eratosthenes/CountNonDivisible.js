3681051de41c1536ed0df96b78075e3443d521d2
Brody Chen <rufengch@gmail.com>
2017-07-22 17:14:40 -0700

    Better CountNonDivisible

--- lesson_11_sieve_of_eratosthenes/CountNonDivisible.js
+++ lesson_11_sieve_of_eratosthenes/CountNonDivisible.js
@@ -1,4 +1,4 @@
-function solution(A) {
+function mySolution(A) {
     // write your code in JavaScript (Node.js 6.4.0)
     const counters = [];
 
@@ -33,5 +33,36 @@ function solution(A) {
         result[i] = A.length - divisors[A[i]] || 0;
     }
     
+    return result;
+}
+
+function solution(A) {
+    const N = A.length;
+
+    const counters = [];
+    A.forEach(a => {
+        counters[a] = counters[a] || 0;
+        counters[a]++;
+    });
+
+    const nonDivisors = [];
+    for (let i = 0; i < counters.length; i++) {
+        nonDivisors[i] = N;
+    }
+
+    // time complexity: O(N*logN)
+    for (let i = 0; i < counters.length; i++) {
+        if (counters[i]) {
+            for (let j = i; j < nonDivisors.length; j += i) {
+                nonDivisors[j] -= counters[i];
+            }
+        }
+    }
+
+    const result = [];
+    for (let i = 0; i < N; i++) {
+        result[i] = nonDivisors[A[i]];
+    }
+
     return result;
 }
